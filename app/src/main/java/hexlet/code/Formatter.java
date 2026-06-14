13405b2339381db61054da7f3bb855545a62afa3
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-06-14 19:45:49 +0300

    refactoring Formatter.java part2

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -55,27 +55,6 @@ public class Formatter {
     }
 
     private static String stylish(Map<String, Object> map1, Map<String, Object> map2) throws Exception {
-//        Set<String> keys = new TreeSet<>();
-//        keys.addAll(map1.keySet());
-//        keys.addAll(map2.keySet());
-//
-//        List<String> lines = new ArrayList<>();
-//        for (String key : keys) {
-//            boolean inFirst = map1.containsKey(key);
-//            boolean inSecond = map2.containsKey(key);
-//            Object value1 = map1.get(key);
-//            Object value2 = map2.get(key);
-//
-//            String type;
-//            if (inFirst && !inSecond) {
-//                type = "removed";
-//            } else if (!inFirst && inSecond) {
-//                type = "added";
-//            } else if (inFirst && inSecond && Objects.equals(value1, value2)) {
-//                type = "unchanged";
-//            } else {
-//                type = "changed";
-//            }
         List<DiffFormat> diffLines = formatRecord(map1, map2);
         List<String> lines = new ArrayList<>();
         for (DiffFormat format : diffLines) {
@@ -95,43 +74,27 @@ public class Formatter {
     }
 
     private static String plain(Map<String, Object> map1, Map<String, Object> map2) throws Exception {
-        Set<String> keys = new TreeSet<>();
-        keys.addAll(map1.keySet());
-        keys.addAll(map2.keySet());
-
+        List<DiffFormat> diffLines = formatRecord(map1, map2);
         List<String> lines = new ArrayList<>();
-        for (String key : keys) {
-            boolean inFirst = map1.containsKey(key);
-            boolean inSecond = map2.containsKey(key);
-            Object value1 = map1.get(key);
-            Object value2 = map2.get(key);
+        for (DiffFormat format : diffLines) {
+            switch (format.type) {
+                case "removed" -> lines.add("Property '" + format.key + "' was removed");
 
-            String type;
-            if (inFirst && !inSecond) {
-                type = "removed";
-            } else if (!inFirst && inSecond) {
-                type = "added";
-            }  else if (inFirst && inSecond && Objects.equals(value1, value2)) {
-                type = "unchanged";
-            } else {
-                type = "changed";
-            }
+                case "added" -> lines.add("Property '" + format.key + "' was added with value: '"
+                        + format.newValue + "'");
 
-            switch (type) {
-                case "removed" -> lines.add("Property '" + key + "' was removed");
-                case "added" -> lines.add("Property '" + key + "' was added with value: '" + value2 + "'");
-                case "unchanged" -> lines.add("Property '" + key + "' was unchanged. Value '" + value1 + "'");
-                case "changed" -> lines.add("Property '" + key + "' was updated. From '"
-                        + value1 + "' to '" + value2 + "'");
-                default -> throw new Exception("Невозможно сравнить строчку! " + type + ": " + key + " " + value1);
+                case "unchanged" -> lines.add("Property '" + format.key + "' was unchanged. Value '"
+                        + format.oldValue + "'");
+
+                case "changed" -> lines.add("Property '" + format.key + "' was updated. From '" + format.oldValue
+                        + "' to '" + format.newValue + "'");
+
+                default -> throw new Exception("Невозможно сравнить строчку! " + format.type + ": " + format.key + " "
+                        + format.oldValue);
             }
         }
-
-        StringBuilder result = new StringBuilder();
-        for (String line : lines) {
-            result.append(line).append("\n");
-        }
-        return  result.toString();
+        String result = String.join("\n", lines);
+        return result + "\n";
     }
 
     private static String json(Map<String, Object> map1, Map<String, Object> map2) {
