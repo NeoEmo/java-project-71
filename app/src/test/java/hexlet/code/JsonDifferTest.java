99c8e2010c39ea5618abd29543dff0964f0d1a8b
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-06-15 11:01:41 +0300

    Update JsonDifferTest.java

--- app/src/test/java/hexlet/code/JsonDifferTest.java
+++ app/src/test/java/hexlet/code/JsonDifferTest.java
@@ -1,12 +1,43 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JsonDifferTest {
+
     @Test
-    public void testJsonDiffer() throws Exception {
+    public void testJsonDiffer2() throws Exception {
         var format = "json";
-        System.out.println(Differ.generate("file1.json", "file2.json", format));
-        System.out.println(Differ.generate("file3.json", "file4.json", format));
+        var result = """
+                {
+                  "timeout": 50
+                  "verbose": true
+                }""";
+
+        assertEquals(result, Differ.generate("file1.json", "file2.json", format));
+    }
+
+    @Test
+    public void testJsonDiffer3() throws Exception {
+        var format = "json";
+        var result = """
+                {
+                  "chars2": [d, e, f]
+                  "checked": false
+                  "default": null
+                  "id": 45
+                  "key2": "value2"
+                  "numbers2": [2, 3, 4, 5]
+                  "numbers4": [4, 5, 6]
+                  "obj1": {
+                    "nestedKey": value,
+                    "isNested": true
+                   }
+                  "setting1": "Some value"
+                  "setting2": 200
+                  "setting3": true
+                }""";
+
+        assertEquals(result, Differ.generate("file3.json", "file4.json", format));
     }
 }
