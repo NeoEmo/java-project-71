6bf491df7c156e7b583f020bd2f04fd90529adc9
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-06-19 13:56:50 +0300

    Update Parser.java

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -7,12 +7,12 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.util.Map;
 
 public class Parser {
-    protected static Map<String, Object> parse(String filePath, String extension) throws Exception {
-        ObjectMapper mapper = switch (extension) {
+    protected static Map<String, Object> parse(String content, String dataType) throws Exception {
+        ObjectMapper mapper = switch (dataType) {
             case "json" -> new ObjectMapper();
             case "yaml", "yml" -> new ObjectMapper(new YAMLFactory());
-            default -> throw new Exception("Неподдерживаемое расширение файла");
+            default -> throw new Exception("Unsupported data format: " + dataType);
         };
-        return mapper.readValue(filePath, Map.class);
+        return mapper.readValue(content, Map.class);
     }
 }
