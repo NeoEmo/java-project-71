f28a210fc9185d39f73481610a7649c236322870
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-06-22 09:51:27 +0300

    rollback JDK 25 to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ application {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_25
-    targetCompatibility = JavaVersion.VERSION_25
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
